346c92546decd442b2582c3b4470a6d17ddbf154
IwnuplyNotTyan <ikissiwnuply@gmail.com>
2026-08-19 21:48:42 +0300

    feat: flake build

--- flake.nix
+++ flake.nix
@@ -1,27 +1,59 @@
 {
-  description = "A very basic flake";
+  description = "🌑 I hate docker >:3";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { nixpkgs, ... }:
-	let
-		system = "x86_64-linux";
-		pkgs = nixpkgs.legacyPackages.${system};
-	in
-	{
-		devShells.${system}.default = pkgs.mkShell {
-			packages = with pkgs; [
-				go
-				gopls
-				gotools
-				golangci-lint			
-			];
+  outputs = { self, nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+	  version = "0.1.0";
+        in
+        {
+          default = pkgs.buildGoModule {
+            pname = "sewz";
+            inherit version;
+            src = self;
+            modules = ./gomod2nix.toml;
 
-			shellHook = ''
-				alias build="go build -o ./bin/hera ."
-			'';
-		};
-  };
+            ldflags = [
+	      "-s"
+	      "-w"
+            ];
+
+            vendorHash = "sha256-N7imleQuAXMp5IyjkpyERNe1X9BuncOsGV8reR/G7o8=";
+
+            meta = {
+              description = "Pretty docker wrapper";
+              homepage = "https://github.com/IwnuplyNotTyan/sewz";
+              mainProgram = "sewz";
+            };
+          };
+	});
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              go
+              gopls
+              gotools
+              golangci-lint
+            ];
+          };
+        });
+    };
 }
